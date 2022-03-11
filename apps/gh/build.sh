07a678a7068acfc6ead73f467a4f7efc5ea7da7d
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-11 10:52:47 +0100

    Add gh

---
+++ apps/gh/build.sh
@@ -0,0 +1,6 @@
+GH_VERSION=$1
+mkdir /gh
+cd /gh
+wget https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_386.tar.gz -O ghcli.tar.gz
+tar --strip-components=1 -xf ghcli.tar.gz
+
