c9d1851111a16efab6bbdcb09fcf045e1debb333
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-11 11:06:22 +0100

    Install git in gh, link gh

--- apps/gh/build.sh
+++ apps/gh/build.sh
@@ -3,4 +3,4 @@ mkdir /gh
 cd /gh
 wget https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_386.tar.gz -O ghcli.tar.gz
 tar --strip-components=1 -xf ghcli.tar.gz
-
+ln -s /gh/bin/gh /usr/local/bin/gh
