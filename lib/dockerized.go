44802d47480eb969c66acfdd568ef1f8abe8bc32
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-17 11:16:17 +0100

    POC - Embedded docker-compose

--- lib/dockerized.go
+++ lib/dockerized.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/compose-spec/compose-go/loader"
-	"github.com/joho/godotenv"
-	"io/ioutil"
+	"github.com/compose-spec/compose-go/cli"
+	"github.com/compose-spec/compose-go/types"
+	"github.com/docker/cli/cli/command"
+	"github.com/docker/cli/cli/flags"
+	"github.com/docker/compose/v2/pkg/api"
+	"github.com/docker/compose/v2/pkg/compose"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"sort"
+	"syscall"
 )
 
 var options = []string{
@@ -54,8 +60,114 @@ func normalizeEnvironment() {
 		os.Setenv("HOME", homeDir)
 	}
 }
+func newSigContext() (context.Context, func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-s
+		cancel()
+	}()
+	return ctx, cancel
+}
+
+func getProject(dockerComposeFilePath string) (*types.Project, error) {
+	options, err := cli.NewProjectOptions([]string{
+		dockerComposeFilePath,
+	},
+		cli.WithDotEnv,
+		cli.WithOsEnv,
+		cli.WithConfigFileEnv,
+	) //, cli.WithDefaultConfigPath
+
+	if err != nil {
+		return nil, nil
+	}
+
+	return cli.ProjectFromOptions(options)
+}
+
+func test(dockerComposeFilePath string) {
+	//apiCli, err := client.NewClientWithOpts(client.FromEnv)
+	//if err != nil {
+	//	panic(err)
+	//}
+	dockerCli, err := command.NewDockerCli()
+	dockerCliOpts := flags.NewClientOptions()
+	err = dockerCli.Initialize(dockerCliOpts)
+	if err != nil {
+		return
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	// call compose
+	var lazyInit = api.NewServiceProxy()
+	lazyInit.WithService(compose.NewComposeService(dockerCli))
+
+	// get from env COMPOSE_FILE
+
+	project, err := getProject(dockerComposeFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, _ := newSigContext()
+
+	serviceName := "go"
+
+	// find service with name "go"
+	service, err := project.GetService(serviceName)
+	service.CustomLabels = map[string]string{}
+	project.Services = []types.ServiceConfig{service}
+
+	opts := api.RunOptions{
+		//Name:              "",
+		Service: serviceName,
+		Command: []string{"version"},
+		//Entrypoint:        nil,
+		Detach:     false,
+		AutoRemove: true,
+		Tty:        true,
+		//WorkingDir:        "",
+		//User:              "",
+		//Environment:       nil,
+		//Labels:            nil,
+		//Privileged:        false,
+		//UseNetworkAliases: false,
+		//NoDeps:            false,
+		//QuietPull:         false,
+		//Index:             0,
+	}
+
+	createOptions := api.CreateOptions{
+		Services:      []string{serviceName},
+		RemoveOrphans: true,
+	}
+	lazyInit.Create(ctx, project, createOptions)
+	exitCode, err := lazyInit.RunOneOffContainer(ctx, project, opts)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if exitCode != 0 {
+		panic(fmt.Errorf("docker-compose exited with code %d", exitCode))
+	}
+
+	//containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//for _, container := range containers {
+	//	fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	//}
+}
 
 func main() {
+
 	normalizeEnvironment()
 
 	command := ""
@@ -81,6 +193,9 @@ func main() {
 	dockerizedRoot := getDockerizedRoot()
 	dockerizedDockerComposeFilePath := filepath.Join(dockerizedRoot, "docker-compose.yml")
 
+	dockerizedDockerComposeFilePath = os.Getenv("COMPOSE_FILE")
+	test(dockerizedDockerComposeFilePath)
+	os.Exit(1)
 	var optionHelp = contains(dockerizedOptions, "--help") || contains(dockerizedOptions, "-h")
 	var optionVerbose = contains(dockerizedOptions, "--verbose") || contains(dockerizedOptions, "-v")
 
@@ -148,7 +263,7 @@ func main() {
 	}
 	// Load in reverse. GoDotEnv does not override vars, this allows runtime env-vars to override the env files.
 	for i := len(envFiles) - 1; i >= 0; i-- {
-		godotenv.Load(envFiles[i])
+		//godotenv.Load(envFiles[i])
 	}
 
 	err = dockerCompose(composeRunArgs)
@@ -191,20 +306,20 @@ func help(dockerComposeFilePath string) error {
 }
 
 func getServices(dockerComposeFilePath string) ([]string, error) {
-	dockerComposeFileBytes, err := ioutil.ReadFile(dockerComposeFilePath)
-	if err != nil {
-		return nil, err
-	}
-	config, err := loader.ParseYAML(dockerComposeFileBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	serviceMaps := config["services"].(map[string]interface{})
+	//dockerComposeFileBytes, err := ioutil.ReadFile(dockerComposeFilePath)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//config, err := loader.ParseYAML(dockerComposeFileBytes)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//serviceMaps := config["services"].(map[string]interface{})
 	var services []string
-	for service := range serviceMaps {
-		services = append(services, service)
-	}
+	//for service := range serviceMaps {
+	//	services = append(services, service)
+	//}
 	return services, nil
 }
 
