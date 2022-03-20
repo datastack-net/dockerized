7a5d4a6ec1729b42c5e03e9bcd97e8a9def06294
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-20 21:35:55 +0100

    feat: Downloads for Linux, Mac and Windows

--- release.config.js
+++ release.config.js
@@ -5,7 +5,31 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github", {
             "assets": [
-                {"path": "release/windows64.zip", "label": "Windows (64bit)", "name": "dockerized-${nextRelease.gitTag}-win64.zip"},
+                {
+                    "path": "release/win32.zip",
+                    "label": "Windows (32bit)",
+                    "name": "dockerized-${nextRelease.gitTag}-win32.zip"
+                },
+                {
+                    "path": "release/win64.zip",
+                    "label": "Windows (64bit)",
+                    "name": "dockerized-${nextRelease.gitTag}-win64.zip"
+                },
+                {
+                    "path": "release/linux-x86_64.zip",
+                    "label": "Linux (x86/64)",
+                    "name": "dockerized-${nextRelease.gitTag}-linux-x86_64.zip"
+                },
+                {
+                    "path": "release/mac-x86_64.zip",
+                    "label": "MacOS (Intel)",
+                    "name": "dockerized-${nextRelease.gitTag}-mac-x86_64.zip"
+                },
+                {
+                    "path": "release/mac-arm64.zip",
+                    "label": "MacOS (Apple Silicon)",
+                    "name": "dockerized-${nextRelease.gitTag}-mac-arm64.zip"
+                },
             ]
         }],
         '@semantic-release/changelog',
