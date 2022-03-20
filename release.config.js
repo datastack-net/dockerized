36feeff24579b012f2e8723ee0b3210f5b033b81
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-20 13:47:07 +0100

    Test releasing with feature branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['master'],
+  branches: ['master', 'feature/semantic-release'],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
