bc3aa94ddc8ab4acba526d6ca349e2d1f2dad9b4
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-20 15:29:25 +0100

    Rename files

--- release.config.js
+++ release.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  branches: ['master', 'feature/semantic-release', 'feature/semantic-release-build'],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      "assets": [
-        {"path": "release/*.zip", "label": "Binary distribution"},
-      ]
-    }],
-    '@semantic-release/changelog',
-    '@semantic-release/git',
-  ],
+    branches: ['master', 'feature/semantic-release', 'feature/semantic-release-build'],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/github", {
+            "assets": [
+                {"path": "release/windows64.zip", "label": "Windows (64bit)", "name": "dockerized-${nextRelease.gitTag}-win64.zip"},
+            ]
+        }],
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+    ],
 };
