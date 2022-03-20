bf976f131cc58177baf37f368a1388bd06c3ba29
Bouke Versteegh <info@boukeversteegh.nl>
2022-03-20 13:49:14 +0100

    chore: semantic release bot (#10)

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  branches: ['master', 'feature/semantic-release'],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
